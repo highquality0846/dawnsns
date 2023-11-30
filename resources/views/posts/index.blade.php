ca3c33ce4fe91475daf820d94dbff80e69a5dbeb
highquaity0846 <highquality0846@yahoo.co.jp>
2023-12-01 00:32:34 +0900

    モーダル作成中

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,16 +19,20 @@
   {{ $post->user_id }}
   {{ $post->posts }}
   {{ $post->created_at }}
-  <div class="pencil" deta-target=  "{{$post->user_id}}">
-  <img src="/images/edit.png" alt="">
+  <div class="life-type">
+    <div class="modal-open" deta-target="{{$post->user_id}}">  <!-- OPEN data"target" -->
+      <img src="/images/edit.png" alt="鉛筆画像">
+    </div>
   </div>
-  <div class="form" id="{{$post->user_id}}">
-  <form action="/post/{{ $post->id }}/update" method='post'>    
-    @csrf
-    <input type='text' name='update_text' placeholder="{{ $post->posts }}" class=''>
-    <button type='submit'>更新</button>
-  </form>
-  </div>
+    <div class="modal-main js-modal" id="{{$post->user_id}}">
+      <div class="modal-inner" >
+        <form class="inner-content" action="/post/{{ $post->id }}/update" method='post'>    
+          @csrf
+          <input type='text' name='update_text' placeholder="{{ $post->posts }}" class=''>
+          <button type='submit'>更新</button>
+        </form> 
+      </div>
+    </div>
 
  <a class="btn btn-danger" href="/post/{{ $post->id }}/delete">削除</a>
 </div>
